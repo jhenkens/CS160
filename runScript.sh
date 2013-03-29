864b983707efd31c403ccb36c1531b56edd2d990
Johan Henkens <johan@henkens.com>
2013-03-29 01:57:48 -0700

    Removed tests (they will be part of seperate repo).

--- runScript.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-./simple < $1 > simple.s
-gcc -m32 -c -o simple.o simple.s
-gcc -m32 -c -o start.o start.c
-gcc -m32 -o start start.o simple.o
-./start
--- start.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int Main();  // note that Main now returns an integer!
-
-int main(int argc, char **argv) {
-    int i = Main();
-    printf("%d\n", i);
-    return 0;
-}
